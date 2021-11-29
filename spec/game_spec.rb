755525fb5690a982188bf44c44984f35679649f4
Tim Stelzner <tim.stelzner@ivx.com>
2021-11-29 13:01:44 +0100

    finished board_spec/ started game_spec

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,1 +1,10 @@
 require_relative '../lib/game'
+
+describe Game do
+  let(:game) { Game.new }
+  context 'play a round' do
+    it 'puts a error statement when number is already taken' do
+      expect(game.play_round(is_taken_number: true)).to output("Invalid Number. Choose another one next time.\n")
+    end
+  end
+end
